3fe2f513b851873bcd9703d177d1e5a3fa37620c
FreeSpaceMan <sar.bartkus@gmail.com>
2021-04-26 00:03:10 +0100

    Second Commit - minor fixes in LanguageAdapter class

--- app/src/main/java/com/example/technicaltaskmobile/fragments/LanguageAdapter.java
+++ app/src/main/java/com/example/technicaltaskmobile/fragments/LanguageAdapter.java
@@ -1,7 +1,6 @@
 package com.example.technicaltaskmobile.fragments;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
@@ -10,20 +9,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.technicaltaskmobile.LanguageItems;
 import com.example.technicaltaskmobile.databinding.ItemLanguageBinding;
 
-import java.util.ArrayList;
-
 public class LanguageAdapter extends RecyclerView.Adapter<LanguageAdapter.LanguageViewHolder> {
-    String[] languageTitles = {"Java", "PHP", "JavaScript", "Python","Kotlin","Golang","C#","C++","C","Groovy"};
-    String[] languageDescritions = {"Java is a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.",
-    "PHP is a general-purpose scripting language especially suited to web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994.",
-    "JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.",
-    "Python is an interpreted high-level general-purpose programming language. Python's design philosophy emphasizes code readability with its notable use of significant indentation.",
-    "Kotlin is a cross-platform, statically typed, general-purpose programming language with type inference. Kotlin is designed to interoperate fully with Java, and the JVM version of Kotlin's standard library depends on the Java Class Library, but type inference allows its syntax to be more concise.",
-    "Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson. Go is syntactically similar to C, but with memory safety, garbage collection, structural typing, and CSP-style concurrency.",
-    "C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines.",
-    "C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language.",
-    "C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system. By design, C provides constructs that map efficiently to typical machine instructions.",
-    "Apache Groovy is a Java-syntax-compatible object-oriented programming language for the Java platform. It is both a static and dynamic language with features similar to those of Python, Ruby, and Smalltalk."};
+    String[][] languageArrayItems = {{"Java","Java is a class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible."},
+            {"PHP","PHP is a general-purpose scripting language especially suited to web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994."},
+            {"JavaScript","JavaScript, often abbreviated as JS, is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions."},
+            {"Python","Python is an interpreted high-level general-purpose programming language. Python's design philosophy emphasizes code readability with its notable use of significant indentation."},
+            {"Kotlin","Kotlin is a cross-platform, statically typed, general-purpose programming language with type inference. Kotlin is designed to interoperate fully with Java, and the JVM version of Kotlin's standard library depends on the Java Class Library, but type inference allows its syntax to be more concise."},
+            {"Golang","Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson. Go is syntactically similar to C, but with memory safety, garbage collection, structural typing, and CSP-style concurrency."},
+            {"C#","C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines."},
+            {"C++","C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language."},
+            {"C","C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system. By design, C provides constructs that map efficiently to typical machine instructions."},
+            {"Groovy","Apache Groovy is a Java-syntax-compatible object-oriented programming language for the Java platform. It is both a static and dynamic language with features similar to those of Python, Ruby, and Smalltalk."}};
 
     @NonNull
     @Override
@@ -36,14 +32,14 @@ public class LanguageAdapter extends RecyclerView.Adapter<LanguageAdapter.Langua
     @Override
     public void onBindViewHolder(@NonNull LanguageViewHolder holder, int position) {
         LanguageItems languageItems = new LanguageItems();
-        languageItems.setTitle(languageTitles[position]);
-        languageItems.setDescription(languageDescritions[position]);
+        languageItems.setTitle(languageArrayItems[position][0]);
+        languageItems.setDescription(languageArrayItems[position][1]);
         holder.bind(languageItems);
     }
 
     @Override
     public int getItemCount() {
-        return languageTitles.length;
+        return languageArrayItems.length;
     }
 
     public class LanguageViewHolder extends RecyclerView.ViewHolder {
